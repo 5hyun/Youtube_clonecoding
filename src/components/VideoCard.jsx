875e1031a2a93dbe953d8ca6cf96ddc96c5418db
5hyun <qhslsl@gmail.com>
2023-03-08 20:18:01 +0900

    Feat: 완성

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,16 +1,27 @@
 import React from "react";
 import { formatAgo } from "../util/date";
+import { useNavigate } from "react-router-dom";
 
-export default function VideoCard({ video }) {
+export default function VideoCard({ video, type }) {
   const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
+  const navigate = useNavigate();
+  const isList = type === "list";
   return (
-    <li>
-      <img className={"w-full"} src={thumbnails.medium.url} alt={title} />
+    <li
+      className={isList ? "flex gap-1 m-2" : ""}
+      onClick={() => {
+        navigate(`/videos/watch/${video.id}`, { state: { video } });
+      }}
+    >
+      <img
+        className={isList ? "w-60 mr-2" : "w-full"}
+        src={thumbnails.medium.url}
+        alt={title}
+      />
       <div>
-        {/* line-clamp은 몇 줄 이상 안나오게 할거냐 */}
-        <p className={"font-semibold my-2 line-clamp-2"}>{title}</p>
-        <p className={"text-sm opacity-80"}>{channelTitle}</p>
-        <p className={"text-sm opacity-80"}>{formatAgo(publishedAt, "ko")}</p>
+        <p className="font-semibold my-2 line-clamp-2">{title}</p>
+        <p className="text-sm opacity-80">{channelTitle}</p>
+        <p className="text-sm opacity-80">{formatAgo(publishedAt, "ko")}</p>
       </div>
     </li>
   );
